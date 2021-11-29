d88ad5d98440a1bce7db9cdd5ca188b455af37af
petracili <petracili7@gmail.com>
2021-11-29 12:07:23 +0000

    Add JS for model 1 in sublime butten

--- static/sendEmail.js
+++ static/sendEmail.js
@@ -15,3 +15,15 @@ function sendMail(contactForm) {
 
     return false;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal1');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal1').modal();
+  });
+          
